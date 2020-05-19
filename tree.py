775ee17894847ed2d075d06a934cb908ff96eab9
Ummaromana Sama <ummaromanasama@gmail.com>
2020-05-19 08:29:24 -0400

    Add files via upload
    Starting off

---
+++ tree.py
@@ -0,0 +1,16 @@
+import turtle
+
+#Initialize screen
+screen = turtle.Screen() 
+
+turtle.title('Fractal Tree')
+
+#Initialize turtle
+myTurtle = turtle.Turtle()
+
+turtle.goto(100,100)
+
+turtle.home()
+
+#Click to exit screen
+screen.exitonclick() 
