fab1cca1a3cecb4d6db2f3dcdfb21e21106cd5d7
Ummaromana Sama <ummaromanasama@gmail.com>
2020-05-22 01:39:06 -0400

    Add files via upload
    Recursion tree

--- tree.py
+++ tree.py
@@ -3,14 +3,32 @@ import turtle
 #Initialize screen
 screen = turtle.Screen() 
 
+#Window name
 turtle.title('Fractal Tree')
 
 #Initialize turtle
 myTurtle = turtle.Turtle()
 
-turtle.goto(100,100)
+#Tree setup
+myTurtle.setheading(90)
+myTurtle.pensize(1)
+myTurtle.speed(700)
 
-turtle.home()
+#Recursion tree
+def tree(x):
+    if(x<10):
+        return
+    else:
+        myTurtle.forward(x)
+        myTurtle.left(30)
+        tree(3*x/4)
+        myTurtle.right(60)
+        tree(3*x/4)
+        myTurtle.left(30)
+        myTurtle.backward(x)
+
+#Call function
+tree(100)
 
 #Click to exit screen
 screen.exitonclick() 
