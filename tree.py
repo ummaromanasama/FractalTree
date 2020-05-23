100b07c2a6d7a4a83e68be63b3d68710bd93d438
Ummaromana Sama <ummaromanasama@gmail.com>
2020-05-23 00:08:37 -0400

    Add files via upload
    Flowers on tree

--- tree.py
+++ tree.py
@@ -11,8 +11,10 @@ myTurtle = turtle.Turtle()
 
 #Tree setup
 myTurtle.setheading(90)
+myTurtle.pencolor((0.2, 0.8, 0.55))
 myTurtle.pensize(1)
 myTurtle.speed(700)
+
 
 #Recursion tree
 def tree(x):
@@ -26,6 +28,15 @@ def tree(x):
         tree(3*x/4)
         myTurtle.left(30)
         myTurtle.backward(x)
+        flower()
+
+#Flowers for the tree
+def flower():
+    #Flower
+    myTurtle.color("pink", "pink")
+    myTurtle.begin_fill()
+    myTurtle.circle(5)
+    myTurtle.end_fill()
 
 #Call function
 tree(100)
