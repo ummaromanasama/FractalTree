f7d738551f89b1f41f7f9bb245082332705b50d9
Ummaromana Sama <ummaromanasama@gmail.com>
2020-05-23 23:35:18 -0400

    Add files via upload
    Colors for flowers and branches are sperate

--- tree.py
+++ tree.py
@@ -1,19 +1,21 @@
 import turtle
 
 #Initialize screen
-screen = turtle.Screen() 
+playground = turtle.Screen() 
 
 #Window name
-turtle.title('Fractal Tree')
+playground.title("Fractal Tree")
+playground.screensize(2000,1500)
+# playground.bgcolor("#197278")
 
 #Initialize turtle
 myTurtle = turtle.Turtle()
 
 #Tree setup
 myTurtle.setheading(90)
-myTurtle.pencolor((0.2, 0.8, 0.55))
+myTurtle.pencolor("#774936")
 myTurtle.pensize(1)
-myTurtle.speed(700)
+myTurtle.speed(1000)
 
 #Recursion tree
 def tree(x):
@@ -27,7 +29,9 @@ def tree(x):
         tree(3*x/4)
         myTurtle.left(30)
         myTurtle.backward(x)
+        # no_flower()
         flower()
+        myTurtle.pencolor("#774936")
 
 #Flowers for the tree
 def flower():
@@ -36,9 +40,25 @@ def flower():
     myTurtle.begin_fill()
     myTurtle.circle(5)
     myTurtle.end_fill()
+    #Another way to create the flowers
+    # myTurtle.dot()
 
+#Use to get the coordinates of the turtle
+def myTurtle_location():
+    location = myTurtle.pos()           
+    myTurtle.write(str(location), True) 
+    print(location)     
+
+#Get ride of flowers in unnecessary locations
+#Think I'm going to need to combine the flower() and
+#no_flower because they go hand in hand
+def no_flower():
+    if (myTurtle.ycor() < 100):  
+        myTurtle.penup()
+    
+    
 #Call function
 tree(100)
 
 #Click to exit screen
-screen.exitonclick() 
+playground.exitonclick() 
