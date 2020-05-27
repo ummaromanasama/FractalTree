d2758cc933dbd8526e8185db3a042901e7c2162f
Ummaromana Sama <ummaromanasama@gmail.com>
2020-05-27 00:07:54 -0400

    Add files via upload
    Spring flowers

--- tree.py
+++ tree.py
@@ -6,14 +6,14 @@ playground = turtle.Screen()
 #Window name
 playground.title("Fractal Tree")
 playground.screensize(2000,1500)
-# playground.bgcolor("#197278")
+playground.bgcolor("#4ecdc4")
 
 #Initialize turtle
 myTurtle = turtle.Turtle()
 
 #Tree setup
 myTurtle.setheading(90)
-myTurtle.pencolor("#774936")
+myTurtle.pencolor("#33001a")
 myTurtle.pensize(1)
 myTurtle.speed(9000)
 
@@ -29,28 +29,40 @@ def tree(x):
         tree(3*x/4)
         myTurtle.left(30)
         myTurtle.backward(x)
-        flower()
+        flowers()
         myTurtle.pendown()
-        myTurtle.pencolor("#774936")
+        myTurtle.pencolor("#33001a")
 
 #Flowers for the tree
-def flower():
-    #I need to account for the rest of the values
-    #Negative/postive and x/y values
-    if (myTurtle.xcor() > 100):  
+def flowers():
+    if (myTurtle.ycor() < 110):  
         myTurtle.penup()
     else:
-        myTurtle.color("pink", "pink")
-        myTurtle.begin_fill()
-        myTurtle.circle(5)
-        myTurtle.end_fill()
+         spring()
 
-#Use to get the coordinates of the turtle
-def myTurtle_location():
-    location = myTurtle.pos()           
-    myTurtle.write(str(location), True) 
-    print(location)       
-    
+#Spring flowers
+def spring():
+    myTurtle.color("#3d2645", "#3d2645")
+    myTurtle.begin_fill()
+    myTurtle.circle(10)
+    myTurtle.end_fill()
+    myTurtle.left(45)    
+    myTurtle.color("#da4167", "#da4167")
+    myTurtle.begin_fill()
+    myTurtle.circle(8)
+    myTurtle.end_fill() 
+    myTurtle.left(45)
+    myTurtle.color("#f391a0", "#f391a0")
+    myTurtle.begin_fill()
+    myTurtle.circle(6)
+    myTurtle.end_fill() 
+    myTurtle.left(45) 
+    myTurtle.color("#ff9f1c", "#ff9f1c")
+    myTurtle.begin_fill()
+    myTurtle.circle(2)
+    myTurtle.end_fill() 
+    myTurtle.right(135)  
+
 #Call function
 tree(100)
 
